23aa32fa983b4e9e86e336e523abd2c2601d1d24
jenkins-test <gjsl1945@naver.com>
2024-02-15 11:19:43 +0900

    찐막

--- src/main/java/com/example/demo/controller/indexcontroller.java
+++ src/main/java/com/example/demo/controller/indexcontroller.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/login")
-@CrossOrigin(origins = "http://localhost:80")
+@CrossOrigin(origins = "http://www.metahospital.shop:80")
 public class indexcontroller {
 
 
