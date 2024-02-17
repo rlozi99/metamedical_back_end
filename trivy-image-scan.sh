a4e9dbc5f4529b0dc7901fcc38a3767732b86da6
Jiwoo Kim <128121468+im-k-jiwoo@users.noreply.github.com>
2024-02-17 18:54:02 +0900

    test commit

--- trivy-image-scan.sh
+++ trivy-image-scan.sh
@@ -1,17 +1,17 @@
 ## Docker 이미지 이름
-#dockerImageName="medicine/back:latest"
-#
+dockerImageName="medicine/back:latest"
+
 ## Trivy 이미지 스캔 실행
-#docker run --rm -v $WORKSPACE:/root/.cache/ -e TRIVY_GITHUB_TOKEN="$TRIVY_GITHUB_TOKEN" aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
-#
+docker run --rm -v $WORKSPACE:/root/.cache/ -e TRIVY_GITHUB_TOKEN="$TRIVY_GITHUB_TOKEN" aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light $dockerImageName
+
 ## Trivy 스캔 결과 처리
-#exit_code=$?
-#echo "종료 코드: $exit_code"
+exit_code=$?
+echo "종료 코드: $exit_code"
 #
 ## 스캔 결과 확인
-#if [[ "$exit_code" == 1 ]]; then
-#    echo "이미지 스캔에 실패했습니다. 심각한 취약점이 발견되었습니다."
-#    exit 1
-#else
-#    echo "이미지 검사가 통과되었습니다. 심각한 취약점이 발견되지 않았습니다."
-#fi
+if [[ "$exit_code" == 1 ]]; then
+    echo "이미지 스캔에 실패했습니다. 심각한 취약점이 발견되었습니다."
+    exit 1
+else
+    echo "이미지 검사가 통과되었습니다. 심각한 취약점이 발견되지 않았습니다."
+fi
